0e6ee549de4f00acf122fae09d2dd200bfbada9b
Kevin Soliz <kevinsoliz10@gmail.com>
2025-09-24 14:49:41 +0200

    Tarea 2.2

---
+++ Tema2/scripts/main1.js
@@ -0,0 +1,7 @@
+ function saludar(){
+        alert("Hey Holaaaa");
+    }
+
+    function desaparece(){
+        document.getElementById("parrafo1").innerHTML = "";
+    }
---
+++ Tema2/scripts/main2.js
@@ -0,0 +1,8 @@
+function modoNoche() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  }
+function modoDia() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
