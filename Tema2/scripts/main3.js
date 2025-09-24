e4adac33fc31f5ce9596c40448b2afff3a24bf2d
Kevin Soliz <kevinsoliz10@gmail.com>
2025-09-24 14:58:26 +0200

    Tarea 2 lista

---
+++ Tema2/scripts/main3.js
@@ -0,0 +1,8 @@
+function cambiarColor(){
+    document.body.style.color = "yellow";
+    document.body.style.backgroundColor = "dodgerblue";
+}
+function centrarTexto(){
+    document.body.style.textAlign = "center";
+    
+}
