63c9f4b509db4ca9f3cbba3199bb05c6b70305c5
Kevin Soliz <kevinsoliz10@gmail.com>
2025-09-29 14:51:16 +0200

    El arbol de navidad no va

--- Tema2/eloquentJS/triangulo.js
+++ Tema2/eloquentJS/triangulo.js
@@ -1,18 +1,12 @@
 function triangulo(filas){
     for (let i = 1; i <= filas; i++){
         let line = "";
-        let line2 = "";
-        for( let j = filas - 1; j >= i; j--){
-            line += ".";
-            for(let k = 0; k < i ; k++){
-                line2 += "#";
-            }
-
+        for (let j = filas; j > i; j--){
+            line += "#";
         }
-        line += line2;
         console.log(line);
-        
     }
+    
 }
 
 triangulo(7);
