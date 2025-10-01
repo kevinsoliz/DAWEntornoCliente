7569db4abd26fc9826c8cd1a05d365abd97f9a6a
Kevin Soliz <kevinsoliz10@gmail.com>
2025-10-01 14:48:37 +0200

    Habia codigo repetitivo

--- Tema2/main5.js
+++ Tema2/main5.js
@@ -1,15 +1,18 @@
 let nombre = prompt("Introduce tu nombre");
 let edad = prompt("Introduce tu edad");
+let cualidad = "";
 
 if (edad <= 12){
-    console.log(`%c${nombre} tiene ${edad} años y por tanto es niño`, 'color: green; font-weight: bold;')
+    cualidad += "niño";
 }
 else if (edad >= 13 && edad <= 17){
-    console.log(`%c${nombre} tiene ${edad} años y por tanto es adolescente`, 'color: green; font-weight: bold;')
+    cualidad += "adolescente";
 }
 else if (edad >= 18 && edad <= 64) {
-    console.log(`%c${nombre} tiene ${edad} años y por tanto es trabajador`, 'color: green; font-weight: bold;')
+   cualidad += "trabajador";
 }
 else {
-    console.log(`%c${nombre} tiene ${edad} años y por tanto es jubilado`, 'color: green; font-weight: bold;')
+   cualidad += "jubilado";
 }
+
+console.log(`%c${nombre} tiene ${edad} años y por tanto es ${cualidad}.`, 'color: green; font-weight: bold;')
